260645538bae963d557b3b934d0ead0c841e2a07
Mariyana-Katz <mariqnak21@gmail.com>
2024-06-11 16:46:48 -0400

    checked

---
+++ answers/Part 2.sql
@@ -0,0 +1,17 @@
+--What are all the types of pokemon that a pokemon can have?
+
+SELECT * FROM types;
+
+--What is the name of the pokemon with id 45?
+SELECT name  FROM pokemons WHERE id=45;
+
+
+--How many pokemon are there?
+ SELECT COUNT(*) FROM pokemons;
+ 656
+
+
+--How many types are there?
+
+
+--How many pokemon have a secondary type?
